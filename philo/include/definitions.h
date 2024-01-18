a47421f2341895242902a8f9274832c72e1ea390
Ferry Ras <57290903+f-ras@users.noreply.github.com>
2024-01-18 21:17:36 +0100

    prepared definitions

--- philo/include/definitions.h
+++ philo/include/definitions.h
@@ -6,7 +6,7 @@
 /*   By: fras <fras@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/12/04 17:49:54 by fras          #+#    #+#                 */
-/*   Updated: 2024/01/05 14:28:02 by fras          ########   odam.nl         */
+/*   Updated: 2024/01/18 21:17:17 by fras          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,10 +33,19 @@ typedef enum e_input_type
 	UNEXPECTED_NUMBER_OF_ARGUMENTS
 }	t_input_type;
 
-typedef enum e_philo_state
+typedef struct s_main
 {
-	CONTINUE,
-	STOP
-}	t_philo_state;
+	bool			finished;
+	pthread_mutex_t	print_mutex;
+	pthread_mutex_t	eat_mutex;
+	pthread_mutex_t	finish_mutex;
+}	t_main;
+
+typedef struct s_philo
+{
+	int			id;
+	pthread_t	thread;
+}	t_philo;
+
 
 #endif
