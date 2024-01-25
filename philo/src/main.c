76f6a81f8c2b53cd59e1943691cffa9de2f512a0
Ferry Ras <57290903+f-ras@users.noreply.github.com>
2024-01-25 21:04:20 +0100

    refactor

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,7 +6,7 @@
 /*   By: fras <fras@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/12/04 17:03:15 by fras          #+#    #+#                 */
-/*   Updated: 2024/01/25 21:01:59 by fras          ########   odam.nl         */
+/*   Updated: 2024/01/25 21:04:01 by fras          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int	main(int argc, char **argv)
 	if (!general.philos)
 		return (print_error(MALLOC_FAILED), MALLOC_FAILED);
 	if (!init_mutex(&general.mutex))
-		return (print_error(MUTEX_FAILED), free(general.philos), MUTEX_FAILED);
+		return (free(general.philos), print_error(MUTEX_FAILED), MUTEX_FAILED);
 	run_philo(general);
 	destroy_mutex(&general.mutex);
 	free(general.philos);
