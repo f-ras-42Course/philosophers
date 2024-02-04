e21350ed7a41e23a3d9a2f1090acf2bf112095b4
Ferry Ras <57290903+f-ras@users.noreply.github.com>
2024-02-04 13:28:27 +0100

    change to make Linux compatible

--- philo/src/output/status.c
+++ philo/src/output/status.c
@@ -6,7 +6,7 @@
 /*   By: fras <fras@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/01/24 15:35:25 by fras          #+#    #+#                 */
-/*   Updated: 2024/02/03 17:10:38 by fras          ########   odam.nl         */
+/*   Updated: 2024/02/04 13:28:04 by fras          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 uint64_t	print_status(uint8_t id, t_philo_status what, t_mutex_group mutex)
 {
 	pthread_mutex_lock(&mutex.print);
-	printf("%llu %d %s\n", get_time(), id, doing(what));
+	printf("%lu %d %s\n", get_time(), id, doing(what));
 	pthread_mutex_unlock(&mutex.print);
 	return (get_time());
 }
